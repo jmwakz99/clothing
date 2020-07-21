496b4044b32a8f87501d6824c0d9bc073631989d
Johnson Mwakazi <mwanjilajohnson@gmail.com>
2020-07-21 19:54:41 +0300

    I forgot to trace what I have been doing but the last, implemented the header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+import Header from "./components/header/header.component";
+import SHOP from "./pages/shop/shop.component";
 import "./App.css";
-
-const Hatspage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hatspage</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={Hatspage} />
+        <Route path="/shop" component={SHOP} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+
 import Directory from "../../components/directory/directory.component";
 
 import "./homepage.styles.scss";
