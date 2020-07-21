d0e5ddc0d94cd37b01fbbf23830dda069d76f22d
Johnson Mwakazi <mwanjilajohnson@gmail.com>
2020-07-21 23:32:32 +0300

    Added signup page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import FormInput from "../form-input/form-input.component";
+
+import CustomButton from "../custom-button/custom-button.component";
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+  render() {
+    return (
+      <div className="sign-in">
+        <h1>I already have an account!</h1>
+        <span className="title">Sign in with email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            required
+            label="Email"
+          />
+
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+            label="Password"
+          />
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
