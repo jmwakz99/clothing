c2bb8d81efb5ee290f3f2c5b83cfad9933e3f8e1
Johnson Mwakazi <mwanjilajohnson@gmail.com>
2020-07-31 09:48:04 +0300

    added advance route feature on the collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
+import { Route } from "react-router-dom";
 
 import "./shop.styles.scss";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route
+        exact
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      />
     </div>
   );
 };
